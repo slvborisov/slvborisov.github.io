f38da6e1b5f70447778ec8d54223f5230f39dab8
slvborisov <63493822+slvborisov@users.noreply.github.com>
2021-01-14 21:31:16 +0200

    Create exs1.py

---
+++ exs1.py
@@ -0,0 +1,9 @@
+print ("Greetings!")
+name = input("What is your name? ")
+print ("Nice to meet you " +name+"!")
+age = int(input("Now, what is your age? "))
+year = str((2020 - age)+100)
+message = name + ", you will be one hundred in " + year
+print (message)
+repeat = int(input("Now give me a random number: 1-10 "))
+print (message * repeat)
